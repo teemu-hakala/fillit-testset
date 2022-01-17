492d92542dd2b84b4aa1ff4a37f855b012fbbdbe
Teemu Hakala <thakala@c1r6p16.hive.fi>
2022-01-17 19:34:59 +0200

    pad_short functions with board_size 5

--- sources/mains/pad_short/main.c
+++ sources/mains/pad_short/main.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:45:43 by thakala           #+#    #+#             */
-/*   Updated: 2022/01/17 12:10:11 by thakala          ###   ########.fr       */
+/*   Updated: 2022/01/17 15:07:55 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static unsigned char	redundancy_check_ref(unsigned long tetrilong, \
+	unsigned char shift_modulus)
+{
+	unsigned long	checker;
+
+	checker = tetrilong;
+	return ((checker >> shift_modulus) << shift_modulus == tetrilong);
+}
+
+unsigned long	pad_short_ref(unsigned short tetrimino, unsigned short index, \
+	unsigned char board_size)
+{
+	unsigned char	t;
+	unsigned char	tetrimino_line;
+	unsigned long	tetrilong;
+	unsigned char	padding;
+
+	t = 16;
+	tetrilong = 0;
+	if (board_size > 4 && board_size <= 20)
+	{
+		padding = board_size - 4;
+		while (t)
+		{
+			tetrilong <<= 4;
+			t -= 4;
+			tetrimino_line = (tetrimino >> t) & 0b1111;
+			tetrilong = (tetrilong ^ tetrimino_line) << padding;
+		}
+		return (tetrilong << (64 - board_size * 4 % 64));
+	}
+	else if (board_size == 4)
+		return ((((unsigned long)tetrimino) << 48) >> index % board_size);
+	else
+		return ((unsigned long)(-1));
+}
+
 static int	ft_test(const unsigned short tetrimino, \
-	const unsigned char index, \
+	const unsigned short index, \
 	const unsigned char board_size)
 {
+	unsigned long	tetrilong;
+
 	printf(GREEN_BG " BOARD_SIZE %hhu" UNCOLOR "\n", board_size);
-	printf(GREEN_BG " INDEX %hhu" UNCOLOR "\n", index);
+	printf(GREEN_BG " INDEX %hu" UNCOLOR "\n", index);
 	fflush(stdout);
 	printf(BLUE_BG " REFERENCE TETRIMINO" UNCOLOR "\n");
 	fflush(stdout);
@@ -32,21 +71,30 @@ static int	ft_test(const unsigned short tetrimino, \
 	display_tetrimino_short(tetrimino);
 	printf(BLUE_BG " PADDED TETRIMINO" UNCOLOR "\n");
 	fflush(stdout);
-	display_tetrimino_long(pad_short(tetrimino, index, board_size), board_size);
+	tetrilong = pad_short(tetrimino, index, board_size);
+	if (!redundancy_check_ref(tetrilong, index % (board_size * board_size)))
+	{
+		printf(RED_BG " PIECE DOESN'T FIT ON THE BOARD" UNCOLOR "\n");
+		fflush(stdout);
+		display_tetrimino_long(~((unsigned long)pad_short_ref(tetrimino, \
+			index, board_size) >> index % 64), board_size);
+	}
+	else
+		display_tetrimino_long(tetrilong >> index % 64, board_size);
 	printf("\n\n");
 	fflush(stdout);
 	return (0);
 }
 
 static int	ft_test_indices(const unsigned short tetrimino, \
-	const unsigned char board_size)
+	const unsigned short board_size)
 {
-	unsigned char	index;
+	unsigned short	index;
 	unsigned short	max_index;
 
 	index = 0;
 	max_index = board_size * board_size;
-	while (index <= max_index)
+	while (index < max_index)
 		ft_test(tetrimino, index++, board_size);
 	return (0);
 }
@@ -61,8 +109,9 @@ static void	function_loader_for_debugging(void)
 
 int	main(void)
 {
-	unsigned char	board_size;
+	unsigned short	board_size;
 
+	function_loader_for_debugging();
 	board_size = 4;
 	while (board_size <= 20)
 	{
